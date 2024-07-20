1e1e72a4f7feb8f4324b451474b584fc77487f57
kibiruki <+>
2024-07-20 12:44:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+
+
+// index.js
+
 function addingEventListener() {
+  const button = document.getElementById('button');
+  if (button) {
+    button.addEventListener('click', () => {
+      console.log('Button clicked!');
+    });
+  }
 }
+
+// Export the function if you need to require it in your test file
+module.exports = { addingEventListener };
